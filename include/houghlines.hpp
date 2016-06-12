0307898444b4a20dfaeab4f4504023f027cbda94
Vieira Neto <vieiranetoc@gmail.com>
2016-06-12 13:05:23 -0300

    not using HoughLines anymore, found area limits from `findContours` function

--- include/houghlines.hpp
+++ include/houghlines.hpp
@@ -11,7 +11,6 @@ class HoughLines
     HoughLines();
     ~HoughLines();
     void calculateProb(cv::Mat);
-    void calculateStd(cv::Mat);
     cv::Mat getResult();
 
   private:
@@ -19,20 +18,15 @@ class HoughLines
     void drawGreatLines(cv::Mat);
     static void onHoughTrackbar(int, void*);
     static void onCannyTrackbar(int, void*);
-    cv::Mat dst, cdst;
-    std::vector<cv::Vec2f> stdlines;
-    std::vector<cv::Vec4i> lines;
-    std::vector<cv::Vec4i> externalLines;
-    int _minlenght;
-    int _maxgap;
-    int _threshold;
+    cv::Mat dst;
+    cv::Rect boundingRect;
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+
     int _cannythresh1;
     int _cannythresh2;
-    int _lenLim;
-    int _leftLim, _leftDst;
-    int _rightLim, _rightDst;
-    int _bottomLim, _bottomDst;
-    int _topLim, _topDst;
+   
+    int largestArea, largestAreaIndex;
     bool _hasfinishedfor;
 };
 
--- src/houghlines.cpp
+++ src/houghlines.cpp
@@ -2,67 +2,49 @@
 
 HoughLines::HoughLines(){
     cv::FileStorage paramReader("data.yml", cv::FileStorage::READ);
-    auto houghParam = paramReader["hough_parameters"];
-    _maxgap = houghParam["Gap"];
-    _minlenght = houghParam["Lenght"];
-    _threshold = houghParam["Thresh"];
+
     auto cannyParam = paramReader["canny_parameters"];
     _cannythresh1 = cannyParam["Thresh1"];
     _cannythresh2 = cannyParam["Thresh2"];
-    auto limitParam = paramReader["limit_parameters"];
-    _lenLim = limitParam["Norm"];
-    _leftLim = limitParam["left"];
-    _leftDst = limitParam["left_Distance"];
-    _bottomLim = limitParam["Bottom"];
-    _bottomDst = limitParam["Bottom_Distance"];
+
     _hasfinishedfor = false;
+    largestArea = 0;
+    largestAreaIndex = 0;
 }
 
 HoughLines::~HoughLines(){
     cv::FileStorage paramWriter("data.yml", cv::FileStorage::WRITE);
-    paramWriter << "hough_parameters";
-    paramWriter << "{" << "Gap"  << _maxgap;
-    paramWriter        << "Lenght" << _minlenght;
-    paramWriter        << "Thresh" << _threshold << "}";
+
     paramWriter << "canny_parameters";
     paramWriter << "{" << "Thresh1" << _cannythresh1; 
     paramWriter        << "Thresh2" << _cannythresh2 << "}";
-    paramWriter << "limit_parameters";
-    paramWriter << "{" << "Norm" << _lenLim;
-    paramWriter        << "Left" << _leftLim;
-    paramWriter        << "Left_Distance" << _leftDst;
-    paramWriter        << "Bottom" << _bottomLim;
-    paramWriter        << "Bottom_Distance" << _bottomDst << "}";
     paramWriter.release();
 }
 
 void HoughLines::calculateProb(cv::Mat input){
     preProcessor(input);
-    /*cv::createTrackbar( "Hough Gap", "RGB Video", &_maxgap, 1000);
-    cv::createTrackbar( "Hough Lenght", "RGB Video", &_minlenght, 200);
-    cv::createTrackbar( "Hough Thresh", "RGB Video", &_threshold, 200);*/
-    cv::HoughLinesP(dst, lines, 1, CV_PI/180, _threshold, _minlenght, _maxgap);
+    std::vector<std::vector<cv::Point> > newcontours;
+    std::vector<cv::Vec4i> newhierarchy;
+
+    cv::findContours(dst, newcontours, newhierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+    for (uint i = 0; i < newcontours.size(); ++i)
+    {
+        double a = cv::contourArea(newcontours[i], false);
+        if(a > largestArea){
+            largestArea = a;
+            largestAreaIndex = i;
+            boundingRect = cv::boundingRect(newcontours[i]);
+            contours = newcontours;
+            hierarchy = newhierarchy;
+        }
+    }
     drawGreatLines(input);
 }
 
 void HoughLines::drawGreatLines(cv::Mat input){
-    std::vector<cv::Vec4i> bestLines;
-    cv::createTrackbar("Left Lim", "RGB Video", &_leftLim, 300);
-    cv::createTrackbar("LDistance", "RGB Video", &_leftDst, 50);
-    cv::createTrackbar("Bottom Lim", "RGB Video", &_bottomLim, 100);
-    cv::createTrackbar("Distance", "RGB Video", &_bottomDst, 20);
-    for(auto line : lines){
-        if(cv::norm(line) > _lenLim)
-            if(line[0] < _leftLim and (std::abs(line[2] - line[0]) < _leftDst))
-                bestLines.push_back(line);
-            if (line[0] < _bottomLim and (std::abs(line[1] - line[3]) < _bottomDst))
-                bestLines.push_back(line);
-
-    }
-    
-    for(auto bestLine : lines)
-        cv::line(input, cv::Point(bestLine[0], bestLine[1]),
-            cv::Point(bestLine[2], bestLine[3]), cv::Scalar(139,0,0), 3, 8 );
+    auto color = cv::Scalar::all(133);
+    cv::drawContours(input, contours, largestAreaIndex, color, 3, 8, hierarchy);
+    cv::rectangle(input, boundingRect, cv::Scalar(0,255,0),1, 8,0); 
 }
 
 
@@ -78,20 +60,3 @@ cv::Mat HoughLines::getResult(){
     return dst;
 }
 
-void HoughLines::calculateStd(cv::Mat input){
-    preProcessor(input);
-    cv::HoughLines(dst, stdlines, 1, CV_PI/180, 100, 0, 0);
-
-    for (size_t i = 0; i < lines.size(); ++i)
-    {
-        float rho = lines[i][0], theta = lines[i][1];
-        cv::Point pt1, pt2;
-        double a = cv::cos(theta), b = cv::sin(theta);
-        double x0 = a*rho, y0 = b*rho;
-        pt1.x = cvRound(x0 + 1000*(-b));
-        pt1.y = cvRound(y0 + 1000*(a));
-        pt2.x = cvRound(x0 - 1000*(-b));
-        pt2.y = cvRound(x0 - 1000*(a));
-        line(cdst, pt1, pt2, cv::Scalar(255,255,255), 3, CV_AA);
-    }
-}
